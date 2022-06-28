10ab28786f632314126053378cc3f6b1d6a65e57
MinhyeSim <smine0032@gmail.com>
2022-06-28 19:50:51 +0900

    2022-06-28 CNN

--- tf2/hand_writing.py
+++ tf2/hand_writing.py
@@ -1,6 +1,6 @@
 import  numpy as np
 import  matplotlib.pyplot as plt
-from keras import datasets
+from keras import datasets, layers, models
 from tensorflow import keras #임의 처리(?)는 케라스
 
 class Solution:
@@ -14,13 +14,35 @@ class Solution:
 
         # 픽셀 값을 0~1 사이로 정규화합니다.
         self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
+        self.layers = layers
+        self.model = models.Sequential()
+        self.train_labels = train_labels
 
     def solution(self):
-        models = keras.models
-        layers = keras.layers
-        model = models.Sequential()
+        self.modeling()
+        self.training()
+
+    def modeling(self):
+        model = self.model
+        layers = self.layers
+
         model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu')) #손글씨는 이미지로 2D이다. 따라서 해당 함수를 사용하는 것이다
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10, activation='softmax'))
+        model.summary()
+
+    def training(self):
+        model = self.model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        model.fit(self.train_images, self.train_labels, epochs=5)
+
+
+if __name__ == '__main__':
+    Solution().solution()
