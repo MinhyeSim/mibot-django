952e8ae3ee3ce6e63ca4019ccd117a3d6b6fcd4e
MinhyeSim <smine0032@gmail.com>
2022-06-29 11:40:34 +0900

    11:39 api 연결

--- tf2/hand_writing_test.py
+++ tf2/hand_writing_test.py
@@ -1,5 +1,6 @@
 #모델들을 각각 만들어 api로 연결하는 형태를 목표로 둔다.
 # 객체지향으로 코딩하지 말기
+import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from tensorflow import keras
 import tensorflow as tf
@@ -10,3 +11,6 @@ x_test = x_test / 255.0 # 데이터 정규화
 # 모델 불러오기
 model = tf.keras.models.load_model('./save/mnist_model.h5')
 model.summary()
+model.evaluate(x_test, y_test, verbose=2)
+plt.imshow(x_test[20], cmap="gray")
+plt.show()
